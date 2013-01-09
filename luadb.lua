88ee7abab78cbdd64f85de86668840c2146f4237
Darren Clark <djcdude@gmail.com>
2013-01-08 21:52:47 -0700

    moved luadb_src.lua into luadb.lua

--- luadb.lua
+++ luadb.lua
@@ -1,4 +1,92 @@
-local luadb_src = require 'luadb_src'
+-------------------
+-- A simple Lua debugger
+-- 
+-- Usage:
+-- 
+-- Require this file:
+-- local luadb = require 'luadb'
+-- 
+-- Set a breakpoint:
+-- luadb.b() / luadb.breakpoint()
+--
+-------------------
+
+
+-------------------
+-- "Modules"
+-- For the sake of simplicity of integration, I wanted to keep
+-- everything in a single file.  I separate some separate components
+-- below:
+-------------------
+
+-------------------
+-- luadb_src - Gets contents of source code files and chunks
+-- Currently used to show current line of code at prompt
+-------------------
+local luadb_src = (function()
+	local files = {}
+
+	-- returns an array of lines in 'file'
+	local function get_file(file)
+		local file_contents = files[file]
+		if file_contents then
+			return file_contents
+		else
+			local lines = {}
+			local success, line_iter = pcall(function () return io.lines(file) end)
+			if success ~= true then
+				return nil
+			end
+
+			for line in line_iter do
+				lines[#lines + 1] = line
+			end
+			files[file] = lines
+			return lines
+		end
+	end
+
+	-- takes a source string that contains code, and returns an array of lines
+	local function parse_string(str)
+		local lines, pos = {}, 0
+		while pos ~= nil do
+			local match_pos = string.find(str, "\n", pos)
+
+			local substr_end = nil
+			if match_pos then substr_end = match_pos - 1 end
+			
+			lines[#lines + 1] = string.sub(str, pos, substr_end)
+			
+			pos = match_pos
+			if pos then pos = pos + 1 end
+		end
+		return lines
+	end
+
+	local function get_line(source, line_number)
+		local lines
+		if string.sub(source, 1, 1) == "@" then
+			lines = get_file(string.sub(source, 2)) -- strip "@" off before!
+		else
+			lines = parse_string(source)
+		end
+
+		if lines == nil then
+			return nil
+		end
+
+		return lines[line_number]
+	end
+
+	-------------------
+	-- Public interface
+	-------------------
+	local luadb_src = {}
+	luadb_src.get_line = get_line
+
+	return luadb_src
+end)()
+
 
 -------------------
 -- Core
